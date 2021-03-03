831e64679a4ab7c0e86500ae18f10c95d9d0bc34
Jesus Cabrero <cabre@192.168.0.26>
2021-03-03 15:08:20 -0500

    made sure all changes are in repo

--- af-location-service/src/test/java/com/revature/controller/LocationControllerTests.java
+++ af-location-service/src/test/java/com/revature/controller/LocationControllerTests.java
@@ -1,13 +1,32 @@
 package com.revature.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.revature.service.LocationService;
 
 public class LocationControllerTests {
-	
-	
-	
-	
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private LocationService locationService;
+
 	@Test
-	public void checkGetBuildingsAtLocationController() {
+	public void checkGetBuildingsAtLocationControllerValid() throws Exception {
+		this.mockMvc.perform(get("/locations").contentType(MediaType.APPLICATION_JSON).content("{}"))
+				.andExpect(status().isOk());
+
+	}
+
+	@Test
+	public void checkGetBuildingsAtLocationConrtollerInvalid() throws Exception {
+		this.mockMvc.perform(get("/locations").contentType(MediaType.APPLICATION_JSON).content("{}"))
+				.andExpect(status().isUnauthorized());
 	}
 }
